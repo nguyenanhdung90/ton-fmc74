ba0eae6660ec0a1c197655fc8ea31f25906bdd10
dungna <dungna@gmail.com>
2024-11-18 11:58:32 +0700

    remove unused  config enviroment

--- config/services.php
+++ config/services.php
@@ -33,8 +33,6 @@ return [
         'is_main' => env('TON_IS_MAIN'),
         'api_key' => env('TON_IS_MAIN') ? env('TON_API_KEY_MAIN') : env('TON_API_KEY_TEST'),
         'root_wallet' => env('TON_ROOT_WALLET'),
-        'master_jetton_usdt' => env('TON_IS_MAIN') ? env('TON_MASTER_USDT_MAIN') : env('TON_MASTER_USDT_TEST'),
-        'master_jetton_not' => env('TON_IS_MAIN') ? env('TON_MASTER_NOT_MAIN') : env('TON_MASTER_NOT_TEST'),
         'mnemonic' => env('TON_MNEMONIC'),
         'fixed_fee' => env('FIXED_FEE')
     ],
@@ -65,22 +63,22 @@ return [
         [
             "currency" => "USDT",
             "hex_master_address" => "0:b113a994b5024a16719f69139328eb759596c38a25f59028b146fecdc3621dfe",
-            "environment" => "MAIN",
+            "environment" => \App\TON\TonHelper::ENVIRONMENT_MAIN,
         ],
         [
             "currency" => "USDT",
             "hex_master_address" => "0:f997be6d6e162809c60c00fce50f51914c021d259f72f9f808fb9c539c479522",
-            "environment" => "TEST",
+            "environment" => \App\TON\TonHelper::ENVIRONMENT_TEST,
         ],
         [
             "currency" => "NOT",
             "hex_master_address" => "0:2f956143c461769579baef2e32cc2d7bc18283f40d20bb03e432cd603ac33ffc",
-            "environment" => "MAIN",
+            "environment" => \App\TON\TonHelper::ENVIRONMENT_MAIN,
         ],
         [
             "currency" => "NOT",
             "hex_master_address" => "0:119ae171343ec283e1495593ead040616ff60bcf399c42a7afa6ef3ce7b56181",
-            "environment" => "TEST",
+            "environment" => \App\TON\TonHelper::ENVIRONMENT_TEST,
         ],
     ]
 ];
